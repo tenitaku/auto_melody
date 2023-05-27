6ee489c1111f3099d054a72f2eb645bb3176db72
tenitaku <kukutaku23@gmail.com>
2023-05-27 18:22:31 +0900

    return

--- script.js
+++ script.js
@@ -1,36 +1,24 @@
 document.getElementById('generateButton').addEventListener('click', function() {
     var words = document.getElementById('inputWords').value.split(' ');
+    var tempo = words.length * 10; // Adjust tempo based on the number of words
     var tempo = words.length * 10;
-  
+
     // Create a new sequence
     var sequence = new Tone.Sequence(function(time, note) {
       synth.triggerAttackRelease(note, '8n', time);
-    }, generateMelody(words), '4n');
-  
+    }, words, '4n');
+
     // Create a simple synth and connect it to the master output
     var synth = new Tone.Synth().toMaster();
-  
+
+    // Create a loop that plays a note every quarter-note
+    var loop = new Tone.Loop(function(time) {
+      synth.triggerAttackRelease("C4", "8n");
+    }, "4n").start(0);
+
+    // Set the tempo and start the transport
     // Start the sequence
     Tone.Transport.bpm.value = tempo;
     sequence.start(0);
     Tone.Transport.start();
   });
-  
-  // Function to generate a melody based on the words
-  function generateMelody(words) {
-    var melody = [];
-    var scale = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4'];
-  
-    for (var i = 0; i < words.length; i++) {
-      var word = words[i];
-      var note = scale[i % scale.length];
-      var octave = Math.floor(i / scale.length);
-      var duration = word.length * 0.1;
-  
-      melody.push(note + (octave + 4) + ':' + duration);
-    }
-  
-    return melody;
-  }
-  
-  
